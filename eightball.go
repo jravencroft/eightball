71aaab5debdb112b500f78cded7e339967ba7726
jravencroft <jravey@gmail.com>
2016-03-14 13:02:06 +0000

    Iterating through Accept-Language and the map for a match would be more standards compliant.

--- eightball.go
+++ eightball.go
@@ -4,13 +4,14 @@ import "net/http"
 import "flag"
 import "fmt"
 import "math/rand"
+import "strings"
 import "time"
 
 var debug = false
 var nocache bool = false
 var port string = ""
 var path string = ""
-var messages []string 
+var messages map[string][]string 
 
 func config() {
 	flag.BoolVar(&debug, "debug", false, "display debug information")
@@ -21,12 +22,24 @@ func config() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	lang := getLang(r.Header.Get("Accept-Language"))
 	random := rand.New(rand.NewSource(time.Now().Unix()))
-	fmt.Fprint(w, messages[random.Intn(len(messages) - 1)])
+	fmt.Fprint(w, messages[lang][random.Intn(len(messages[lang]) - 1)])
 }
 
-func fillmessages(count int) []string{
-	var out = []string {
+func getLang(header string) string {
+	if strings.HasPrefix(header, "en") {
+		return "en"
+	} else if strings.HasPrefix(header, "it") {
+		return "it"
+	} else {
+		return "en"
+	}
+}
+
+func fillmessages(count int) map[string][]string{
+	out := map[string][]string {}
+	out["en"] = []string {
 		"It is certain",
 		"It is decidedly so",
 		"Without a doubt",
@@ -47,6 +60,29 @@ func fillmessages(count int) []string{
 		"My sources say no",
 		"Outlook not so good",
 		"Very doubtful",
+	}
+
+	out["it"] = []string {
+		"Per quanto posso vedere, sì",
+		"È certo",
+		"È decisamente così",
+		"Molto probabilmente",
+		"Le prospettive sono buone",
+		"I segni indicano di sì",
+		"Senza alcun dubbio",
+		"Sì",
+		"Sì, senza dubbio",
+		"Ci puoi contare",
+		"È difficile rispondere, prova di nuovo",
+		"Rifai la domanda più tardi",
+		"Meglio non risponderti adesso",
+		"Non posso predirlo ora",
+		"Concentrati e rifai la domanda",
+		"Non ci contare",
+		"La mia risposta è no",
+		"Le mie fonti dicono di no",
+		"Le prospettive non sono buone",
+		"Molto incerto",
 	}
 	return out
 }
